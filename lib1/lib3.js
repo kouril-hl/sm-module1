925ed2abd0723694b03c6b5c5535cb1a11558548
David Kouřil <kouril@haug.land>
2024-09-12 08:12:58 +0200

    test3

--- lib1/lib3.js
+++ lib1/lib3.js
@@ -4,3 +4,6 @@ console.log('lib3.js');
 console.log(trimStart('lib3.js from repo1', 'lib3.js '));
 console.log(trimStart('lib3.js from repo1', 'lib3.js '));
 console.log(trimStart('lib3.js from repo1', 'lib3.js '));
+
+
+console.log(trimStart('lib3.js from repo1', 'lib3.js '));
