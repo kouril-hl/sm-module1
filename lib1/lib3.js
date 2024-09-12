fd740d673fb77b3f6926d5a595d70c55de034c64
David Kouřil <kouril@haug.land>
2024-09-12 08:09:10 +0200

    test2

--- lib1/lib3.js
+++ lib1/lib3.js
@@ -3,3 +3,4 @@ import {trimStart} from 'lodash-es';
 console.log('lib3.js');
 console.log(trimStart('lib3.js from repo1', 'lib3.js '));
 console.log(trimStart('lib3.js from repo1', 'lib3.js '));
+console.log(trimStart('lib3.js from repo1', 'lib3.js '));
